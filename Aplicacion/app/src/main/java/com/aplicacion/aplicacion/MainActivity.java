237703faec9a1f5ed509f5eecbf6aadda106ffe3
chriscanostein <christiancano27@hotmail.com>
2017-03-05 23:10:08 -0300

    Aplicacion funcionando ubuntu 16.04

---
+++ Aplicacion/app/src/main/java/com/aplicacion/aplicacion/MainActivity.java
@@ -0,0 +1,22 @@
+package com.aplicacion.aplicacion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.aplicacion.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Osmar Romero -
 Christian Cano -
 Pablo Barrios -
 Junior Godoy
+
+Proyecto Aplicacion
+
+hecho en ubuntu 16.04.2 lts
+android studio 145.35
+
+Api 19 kitkat 4.4.2 (menor)
